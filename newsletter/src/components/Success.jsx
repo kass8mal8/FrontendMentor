645abe1587e8eb34cc929fd6778bee2bd2289d2c
kass8mal8 <kassimaly21@gmail.com>
2024-06-01 09:29:50 +0300

    small modifications

--- newsletter/src/components/Success.jsx
+++ newsletter/src/components/Success.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import icon from "../assets/images/icon-list.svg"
 import Btn from "./Btn";
 
