3d0f0c4c43475de393a00c209598af8ef984cab9
kass8mal8 <kassimaly21@gmail.com>
2024-06-01 14:10:19 +0300

    mobile responsiveness fix

--- newsletter/src/App.jsx
+++ newsletter/src/App.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, ThemeProvider, createTheme } from "@mui/material";
 import "./styles/css/index.css"
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
@@ -7,13 +7,20 @@ import { useState } from "react";
 
 const App = () => {
   const [data, setData] = useState({})
+  const theme = createTheme({
+    typography: {
+      fontFamily: "roboto"
+    }
+  })
   return (  
-    <Box className='container'>
-      <Routes>
-        <Route path="/" element={<Home data={data} setData={setData} />} />
-        <Route path="/success" element={<Success data={data} setData={setData} />} />
-      </Routes>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box className='container'>
+        <Routes>
+          <Route path="/" element={<Home data={data} setData={setData} />} />
+          <Route path="/success" element={<Success data={data} setData={setData} />} />
+        </Routes>
+      </Box>
+    </ThemeProvider>
   );
 }
  
