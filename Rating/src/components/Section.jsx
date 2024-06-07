86b538d36aa72728ec7ae64fcc7350355d2199dc
kass8mal8 <kassimaly21@gmail.com>
2024-06-07 09:58:31 +0300

    rating component

--- Rating/src/components/Section.jsx
+++ Rating/src/components/Section.jsx
@@ -4,20 +4,20 @@ import icon_star from "../assets/images/icon-star.svg";
 import Btn from "./Btn";
 
 const Section = () => {
-    return (  
-        <Box className="section" component={'aside'}>
-            <img src={icon_star} alt="star" />
-               
-            <Typography variant='h5'>How did we do?</Typography>
-            <Typography variant='body1'>
-                Please let us know how we did with your support request. All feedback is appreciated 
-                to help us improve our offering!
-            </Typography>
+  return (
+    <Box className="section" component={"aside"}>
+      <img src={icon_star} alt="star" />
 
-            <SelectRate />
-            <Btn />
-        </Box>
-    );
-}
- 
+      <Typography variant="h5">How did we do?</Typography>
+      <Typography variant="body1">
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </Typography>
+
+      <SelectRate />
+      <Btn />
+    </Box>
+  );
+};
+
 export default Section;
--- Rating/src/components/SelectRate.jsx
+++ Rating/src/components/SelectRate.jsx
@@ -2,25 +2,30 @@ import { useState, useMemo, useContext } from "react";
 import { RatingContext } from "../App";
 
 const SelectRate = () => {
-    const nums = useMemo(() => [1, 2, 3, 4, 5], []);
-    const [count, setCount] = useState(null);
-    const { setSelected } = useContext(RatingContext)
+	const nums = useMemo(() => [1, 2, 3, 4, 5], []);
+	const [count, setCount] = useState(null);
+	const { setSelected } = useContext(RatingContext);
 
-    return (  
-        <ul>
-            {nums.map(num => (
-                <li 
-                    key={num} 
-                    onClick={() => {setCount(num); setSelected(num);}} 
-                    style={{ 
-                        backgroundColor: num === count && 'white', 
-                        color: num === count && 'hsl(216, 12%, 8%)'
-                    }}
-                > {num} 
-                </li>
-            ))}
-        </ul>
-    );
-}
- 
+	return (
+		<ul>
+			{nums.map((num) => (
+				<li
+					key={num}
+					onClick={() => {
+						setCount(num);
+						setSelected(num);
+					}}
+					style={{
+						backgroundColor: num === count && "white",
+						color: num === count && "hsl(216, 12%, 8%)",
+					}}
+				>
+					{" "}
+					{num}
+				</li>
+			))}
+		</ul>
+	);
+};
+
 export default SelectRate;
